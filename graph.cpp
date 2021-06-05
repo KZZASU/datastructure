ce3fb9a4b9674c704b12985e5f29eb8e857f8d55
kzzasu <seongugoh@gmail.com>
2021-06-05 22:05:31 +0900

    Prim's Algorithm

--- graph.cpp
+++ graph.cpp
@@ -38,23 +38,31 @@ void Graph::SetEdge(Edge e)
 	Edges_.push_back(e);
 }
 
+void Graph::GetEdge()
+{
+	for (auto e : Edges_)
+	{
+		cout << e.v1_ << ' ' << e.v2_ << ' ' << e.w_ << '\n';
+	}
+}
+
 vector<vector<weight>> Graph::adjacent_matrix()
 {
 	vector<vector<weight>> amat{};
 
-	size_t size_ = vertices_.size() + 1;
-	vector<weight> dummy(size_ + 1);
+	size_t size_ = vertices_.size();
+	vector<weight> dummy(size_ + 1, INT_MAX);
 	amat.push_back(dummy);
 
 	for (size_t j{ 1 }; j < size_ + 1; j++)
 	{
-		vector<weight> row(size_ + 1);
+		vector<weight> row(size_ + 1, INT_MAX);
 		amat.push_back(row);
 	};
 
 	for (Edge e : Edges_)
 	{
-		if ((amat[e.v1_][e.v2_] == 0) && (amat[e.v2_][e.v1_]) == 0)
+		if ((amat[e.v1_][e.v2_] == INT_MAX) && (amat[e.v2_][e.v1_]) == INT_MAX)
 		{
 			amat[e.v1_][e.v2_] = e.w_;
 			amat[e.v2_][e.v1_] = e.w_;
@@ -65,10 +73,13 @@ vector<vector<weight>> Graph::adjacent_matrix()
 	{
 		for (size_t j{ 1 }; j < size_ + 1; j++)
 		{
-			cout << amat[i][j] << ' ';
+			if (amat[i][j] == INT_MAX) { cout << 0 << ' '; }
+			else{ cout << amat[i][j] << ' '; }
+
 		}
 		cout << '\n';
 	}
+	cout << '\n';
 
 	return amat;
 }
@@ -87,8 +98,45 @@ Graph Graph::minimum_span()
 	}
 	Span.SetEdge(min_edge);
 
-	int* near = new int[vertices_.size() + 1];
+	vector<vector<weight>> amat{ this->adjacent_matrix() };
 
+	vector<weight> near(vertices_.size() + 1, min_edge.v2_);
+
+	amat[min_edge.v1_][min_edge.v1_] = 0;
+	amat[min_edge.v2_][min_edge.v2_] = 0;
+
+	for (size_t i{ 1 }; i < vertices_.size() + 1; i++)
+	{
+		if (amat[min_edge.v1_][i] < amat[min_edge.v2_][i])
+		{
+			near[i] = min_edge.v1_;
+		}
+	}
+	
+	while (Span.Edges_.size() < Span.vertices_.size()-1)
+	{
+		int next_x = 0;
+		for (size_t i{ 1 }; i < vertices_.size() + 1; i++)
+		{
+			if ((amat[next_x][near[next_x]] > amat[i][near[i]]) && (i != near[i]))
+			{
+				next_x = i;
+			}
+		}
+
+		Span.SetEdge(next_x, near[next_x], amat[next_x][near[next_x]]);
+		amat[next_x][next_x] = 0;
+
+		for (size_t i{ 1 }; i < vertices_.size() + 1; i++)
+		{
+			if (amat[i][next_x] <= amat[i][near[i]])
+			{
+				near[i] = next_x;
+			}
+		}
+	};
+	
+	return Span;
 }
 
 
@@ -162,10 +210,16 @@ void dfs(unordered_map<int, vector<int>> G, int start)
 
 int main()
 {
-
     unordered_map<int, vector<int>> g{ {1,{2,3,4}},{2,{1,3}}, {3,{1,2,4,5}}, {4,{1,3}}, {5,{3,4,6,7}}, {6,{5}}, {7,{5}} };
-    vector<vector<int>> edges{ {3,1},{10,1},{3,1},{10,1},{1,8},{5,2} };
-    /*bfs(10, 6, edges, 3);*/
     unordered_map<int, vector<int>> g2{ {1,{2,3}}, {2,{1,4,5}},{3,{1,6,7}},{4,{2}},{5,{2}},{6,{3}},{7,{3}} };
+
+	vector<Edge> edges{ {1,6,5}, {1,2,25}, {2,7,6},{2,3,9},{3,4,10},{4,5,12},{4,7,11},{5,6,40},{5,7,16}};
+
+	Graph G{ 7 };
+
+	G.SetEdge(edges);
+	Graph Span_G{ G.minimum_span() };
+
+	Span_G.GetEdge();
     return 0;
 }
